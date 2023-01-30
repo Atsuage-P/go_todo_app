86d9184a68bdfc35a178cd62cf0e0de3bce2b833
atsuage <atsuage.programmer@gmail.com>
2023-01-30 23:05:51 +0900

    リファクタ

--- main.go
+++ main.go
@@ -5,12 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"go_todo_app/config"
-
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -33,29 +30,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバを起動する
-	eg.Go(func() error {
-		// http.ErrServerClosedはhttp.Server.Shutdown()が正常に終了したことを示すので異常ではない
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-				log.Printf("failed to close: %+v", err)
-				return err
-			}
-		return nil
-	})
-
-	// チャネルからの通知(終了通知)を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	// Goメソッドで起動した別ゴルーチンの終了を待つ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
